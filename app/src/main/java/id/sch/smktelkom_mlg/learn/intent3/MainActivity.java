c6b6148cbdbb09bd030e6f011bf544ea7df3d99c
rioranup <rioranu2@gmail.com>
2016-11-18 02:05:08 +0700

    Intent Camera (Photo)

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.imageViewPhone).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialPhoneNumber("0341712500");
+                dialPhoneNumber("082139794255");
             }
         });
 
